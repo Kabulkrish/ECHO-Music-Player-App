29ea0a52262358b1aebd14855902b48df1696039
Manisha Mandal <slmanisha14@gmail.com>
2018-03-13 14:42:54 +0530

    START

---
+++ adapters/NavigationDrawerAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.manisha.echo.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by manisha on 06-02-2018.
+ */
+class NavigationDrawerAdapter:RecyclerView.Adapter<NavigationDrawerAdapter.NavViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): NavViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: NavViewHolder?, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class NavViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
